7816452d6ae4ddd3bdef8ab8676443aff8f2d6a7
arunapeddineni19 <47892934+arunapeddineni19@users.noreply.github.com>
2019-02-26 22:53:02 +0530

    Create inertval.py

---
+++ inertval.py
@@ -0,0 +1,10 @@
+lower,upper=(int(x)for x in input().split())
+l=[]
+for num in range(lower,upper+1):
+  if num>1:
+    for x in range(2,num):
+      if num%x==0:
+        break
+    else:
+      l.append(num)
+print(*l)        
