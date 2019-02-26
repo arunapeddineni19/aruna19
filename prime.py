2a7d4cdd1f3574c3853295ad522c01eab207b5f7
arunapeddineni19 <47892934+arunapeddineni19@users.noreply.github.com>
2019-02-26 22:27:47 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,14 @@
+num=int(input())
+if num<=1000:
+   if num>1:
+
+      for x in range(2,num):
+           if num%x==0:
+              print("no")
+              break
+      else:
+        print("yes")        
+ 
+         
+ 
+   
